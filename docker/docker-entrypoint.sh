06bbce724c1a1e84c61166cd57c30b6eea6066d2
jzl1 <34795003+jzl1@users.noreply.github.com>
2023-01-17 21:16:08 -0500

    Update docker-entrypoint.sh

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,30 +1,6 @@
 #!/bin/bash
 export HOME=/config
 
-for modPath in /data/Stardew/Stardew\ Valley/Mods/*/
-do
-  mod=$(basename "$modPath")
-
-  # Normalize mod name ot uppercase and only characters, eg. "Always On Server" => ENABLE_ALWAYSONSERVER_MOD
-  var="ENABLE_$(echo "${mod^^}" | tr -cd '[A-Z]')_MOD"
-
-  # Remove the mod if it's not enabled
-  if [ "${!var}" != "true" ]; then
-    echo "Removing ${modPath} (${var}=${!var})"
-    rm -rf "$modPath"
-    continue
-  fi
-
-  if [ -f "${modPath}/config.json.template" ]; then
-    echo "Configuring ${modPath}config.json"
-
-    # Seed the config.json only if one isn't manually mounted in (or is empty)
-    if [ "$(cat "${modPath}config.json" 2> /dev/null)" == "" ]; then
-      envsubst < "${modPath}config.json.template" > "${modPath}config.json"
-    fi
-  fi
-done
-
 # Run extra steps for certain mods
 /opt/configure-remotecontrol-mod.sh
 
@@ -38,4 +14,4 @@ sed -i -e 's/env TERM=xterm $LAUNCHER "$@"$/env SHELL=\/bin\/bash TERM=xterm xte
 
 bash -c "/data/Stardew/Stardew\ Valley/StardewValley"
 
-sleep 233333333333333
+sleep infinity
